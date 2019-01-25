9c18a3d73ace5cb8b34b0740f46044f93cb168b5
kyc1109 <kyc1109@gmail.com>
2019-01-25 16:00:39 +0800

    Create new file
    1st

---
+++ auto_run.py
@@ -0,0 +1,24 @@
+#!python3
+# -*- coding: UTF-8 -*-
+import sys, os, time, datetime, ctypes, subprocess
+def auto_run():
+    filename=os.path.basename(__file__).replace(".py", ".exe")
+    filefullpathname=os.getcwd()+"\\"+filename#sys.argv[0] is not stable
+    regkey="HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+    print("cwd:", os.getcwd())
+    #os.chdir(os.environ['USERPROFILE']+"\\Desktop") #Change the current working directory to Desktop
+    #print("cwd: ", os.getcwd())
+    #print("regkey: ", regkey)
+    print("filename:",filename)
+    print("filefullpathname:",filefullpathname)
+    try:
+        #print("regkey: ","reg add "+regkey+" /v stress_"+filename.replace(".exe","")+" /t REG_SZ /d "+filefullpathname+" /f")
+        os.system("reg add "+regkey+" /v stress_"+filename.replace(".exe","")+" /t REG_SZ /d "+filefullpathname+" /f")
+        print("auto run reg ok")
+        os.system("timeout /t 10")
+    except:
+        print("auto run reg failed !!!")
+        os.system("timeout /t 30")
+
+if __name__ == "__main__":  # Start from here
+    auto_run()
